a58b21422d57d0e900c98f2e4d9aac7e232b3826
din-sil <dinsil221@gmail.com>
2022-07-04 22:45:05 -0300

    Adding my solution to  homework

---
+++ Mod3/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+B
+exceptions/pom.xml,e/0/e0c79480af9e6d477b0f59498eb56fbe4782f1df
---
+++ Mod3/mvn/src/main/java/br/com/helloworld/mvn/MvnApplication.java
@@ -0,0 +1,13 @@
+package br.com.helloworld.mvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnApplication.class, args);
+	}
+
+}
